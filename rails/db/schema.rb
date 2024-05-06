55f2a92ebbbd05c8c9e4b56c5e231b20cc418121
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-05-06 14:43:37 +0900

    Post Facilityテーブルの作成

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,5 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_06_054009) do
+  create_table "facilities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address", null: false
+    t.string "content", null: false
+    t.float "latitude", null: false
+    t.float "longitude", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "facility_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["facility_id"], name: "index_posts_on_facility_id"
+  end
+
+  add_foreign_key "posts", "facilities"
 end
