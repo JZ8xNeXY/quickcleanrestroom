b087ab0cd5a302223e9393ec0b61cf9bd82df72a
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-05-08 20:29:00 +0900

    next.config.jsの修正

--- next/.next/static/chunks/webpack.js
+++ next/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7237f75d0b1bd017"; }
+/******/ 		__webpack_require__.h = function() { return "7dd43811aa08dba2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
