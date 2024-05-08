dd7c3f63ba9259cba4e749a0e7d8f2d336aae8fb
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-05-08 12:33:40 +0900

    トップページの作成’ ; git commit -m トップページの作成’
    git ignoreファイルの修正
    
    gitignoreファイルの修正

--- next/.next/static/chunks/webpack.js
+++ next/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7dd43811aa08dba2"; }
+/******/ 		__webpack_require__.h = function() { return "7237f75d0b1bd017"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
