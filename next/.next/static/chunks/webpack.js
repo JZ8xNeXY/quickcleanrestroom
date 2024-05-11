a83aa0d4111c31116193b6d07ad9296eb085cc2a
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2024-05-12 06:39:16 +0900

    現在地アイコンの重複を避ける

--- next/.next/static/chunks/webpack.js
+++ next/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e12ad56d39fd6c5d"; }
+/******/ 		__webpack_require__.h = function() { return "318f4870bf93116c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next/src/utils/userGeoLocation.ts
+++ next/src/utils/userGeoLocation.ts
@@ -5,6 +5,8 @@ interface UserGeoLocationProps {
   setCurrentUserPos: (pos: { lat: number; lng: number }) => void
 }
 
+let userPosImg: HTMLImageElement
+
 export const userGeoLocation = ({
   map,
   setCurrentUserPos,
@@ -17,35 +19,8 @@ export const userGeoLocation = ({
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         }
-
-        const userPosImg = document.createElement('img')
-        userPosImg.src = '/userposition.png'
-        userPosImg.width = 75
-        userPosImg.height = 75
-        userPosImg.classList.add('bounce')
-
-        const { AdvancedMarkerElement } = (await google.maps.importLibrary(
-          'marker',
-        )) as google.maps.MarkerLibrary
-
-        new AdvancedMarkerElement({
-          map,
-          position: UserPos,
-          title: 'Your Location',
-          content: userPosImg,
-        })
-
-        setCurrentUserPos(UserPos)
-
-        if (map) {
-          map.setCenter(UserPos)
-        }
-      },
-      () => {
-        // GPS機能で現在地を取得できない場合、東京駅の位置を使用
-        const handleLocationError = async (
-          tokyoStationPos = { lat: 35.681236, lng: 139.767125 },
-        ) => {
+        //現在地アイコンの重複を避けるため
+        if (!userPosImg) {
           const userPosImg = document.createElement('img')
           userPosImg.src = '/userposition.png'
           userPosImg.width = 75
@@ -58,12 +33,44 @@ export const userGeoLocation = ({
 
           new AdvancedMarkerElement({
             map,
-            position: tokyoStationPos,
+            position: UserPos,
             title: 'Your Location',
             content: userPosImg,
           })
 
-          setCurrentUserPos(tokyoStationPos)
+          setCurrentUserPos(UserPos)
+        }
+
+        if (map) {
+          map.setCenter(UserPos)
+        }
+      },
+      () => {
+        // GPS機能で現在地を取得できない場合、東京駅の位置を使用
+        const handleLocationError = async (
+          tokyoStationPos = { lat: 35.681236, lng: 139.767125 },
+        ) => {
+          //現在地アイコンの重複を避けるため
+          if (!userPosImg) {
+            userPosImg = document.createElement('img')
+            userPosImg.src = '/userposition.png'
+            userPosImg.width = 75
+            userPosImg.height = 75
+            userPosImg.classList.add('bounce')
+
+            const { AdvancedMarkerElement } = (await google.maps.importLibrary(
+              'marker',
+            )) as google.maps.MarkerLibrary
+
+            new AdvancedMarkerElement({
+              map,
+              position: tokyoStationPos,
+              title: 'Your Location',
+              content: userPosImg,
+            })
+
+            setCurrentUserPos(tokyoStationPos)
+          }
 
           if (map) {
             map.setCenter(tokyoStationPos)
